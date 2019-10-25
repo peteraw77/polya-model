b4ceb54c31bb62b5ead2e60e5435cf2595ff1a0e
Peter Wright <pewright77@gmail.com>
2019-10-25 14:18:23 -0400

    Instantiate the Node objects

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import networkx as nx
 
-def construct_barabasi():
+def construct_barabasi(size):
     # gotta learn what the second parameter means
-    graph = nx.barabsi_albert_graph(100, 5)
+    graph = nx.barabsi_albert_graph(size, 5)
 
     return graph.edges
+
+# size is the number of nodes
+def simulation(size):
+    edges = construct_barabasi(size)
+    nodes = [None for x in range(size)]
+
+    # build the node objects
+    for edge in edges:
+        # add connection to first node
+        node = nodes[edge[0]]
+        if node == None:
+            node = MemorylessNode([edge[1]])
+        else:
+            node.neighborhood = node.neighborhood.append(edge[1])
+
+        # second node
+        node = nodes[edge[1]]
+        if node == None:
+            node = MemorylessNode([edge[0]])
+        else:
+            node.neighborhood = node.neighborhood.append(edge[0])
+
+        # run the simulation
