830eccbf7744de76abbd36ee6a852daf72c64a05
Peter Wright <pewright77@gmail.com>
2019-11-01 15:23:47 -0400

    Fix infinite memory node

--- network.py
+++ network.py
@@ -9,10 +9,10 @@ class Node:
 
     def draw(self, nodes):
         # construct super urn
-        total_red = red_balls
-        total_black = black_balls
+        total_red = self.red_balls
+        total_black = self.black_balls
 
-        for address in neighborhood:
+        for address in self.neighborhood:
             node = nodes[address]
             total_red = total_red + node.red_balls
             total_black = total_black + node.black_balls
@@ -21,9 +21,9 @@ class Node:
         red_prob = total_red / (total_red + total_black)
         # should this be <= or < ?
         if (random.uniform(0,1) <= red_prob):
-            red_balls = red_balls + delta
+            self.red_balls = self.red_balls + self.delta
         else:
-            black_balls = black_balls + delta
+            self.black_balls = self.black_balls + delta
 
 class MemorylessNode:
     def __init__(self, neighborhood=[], red_balls=1, black_balls=1, memory=3, delta=3):
