ed1af9264b746a05d6ef7da2bcbffa8e0ee43880
Peter Wright <pewright77@gmail.com>
2019-11-07 13:24:50 -0500

    Plot infection of node 0 and entire network

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from copy import deepcopy
-from polya import FiniteNode
+from polya import FiniteNode, network_infection_rate
+import matplotlib.pyplot as plt
 
 def construct_barabasi(size):
     # gotta learn what the second parameter means
@@ -29,9 +30,13 @@ def simulation(size, runtime):
 
     # run the simulation
     infection_node_zero = []
+    avg_infection_rate = []
     for t in range(runtime):
+        # update infection rates
         node_zero_red, node_zero_black = nodes[0].construct_super_urn(nodes)
         infection_node_zero.append(node_zero_red / (node_zero_red + node_zero_black))
+        avg_infection_rate.append(network_infection_rate(nodes))
+
         # remove values that are out of network's memory
         for node in nodes:
             node.update()
@@ -44,7 +49,14 @@ def simulation(size, runtime):
         nodes = new_nodes
 
     # show off results somehow
-    print(infection_node_zero)
+    plt.figure('Node0')
+    plt.plot(range(runtime), infection_node_zero)
+    plt.title('Infection rate for Node 0')
+
+    plt.figure('Network')
+    plt.plot(range(runtime), avg_infection_rate)
+    plt.title('Average Infection Rate of Network')
+    plt.show()
 
 if __name__ == '__main__':
     simulation(10, 1000)
