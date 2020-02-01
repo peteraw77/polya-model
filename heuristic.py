c977fe3ded50e6390962c9d1ad383c03729f6ba2
Jessica Landon <15jdl4@queensu.ca>
2020-01-31 20:42:34 -0500

    Fixed heuristics.

--- heuristic.py
+++ heuristic.py
@@ -26,7 +26,7 @@ def build_network(adjacency):
         # Will be updated at each time step during simulation, just initialize array so it works, not used
         delta_black = []
         for i in range(len(adjacency)):
-            delta_black.append(0.5)
+            delta_black.append(delta_red)
             
     # build the node objects
     for i in range(len(adjacency)):
@@ -47,7 +47,7 @@ def get_delta_black(nodes, adjacency, delta_red):
         degrees.append(graph.degree[i])
 
      # Budget for curing is same as total number of delta_red added to network
-    budget = delta_red*len(nodes)
+    budget = delta_red*graph.number_of_nodes()
 
     delta_black = []
     
@@ -57,21 +57,22 @@ def get_delta_black(nodes, adjacency, delta_red):
             delta_black.append(budget*prev_net_exp[i] / sum(prev_net_exp))
 
     elif TYPE == 'closeness':
-        for i in range(len(nodes)):
+        for i in range(graph.number_of_nodes()):
             delta_black.append(budget*centralities[i] / sum(centralities.values()))
+            
     elif TYPE == 'degree':
-        for i in range(len(nodes)):
+        for i in range(graph.number_of_nodes()):
             delta_black.append(budget*degrees[i] / sum(degrees))
             
     elif TYPE == 'centrality':
         # Combine closeness and degree
-        for i in range(len(nodes)):
+        for i in range(graph.number_of_nodes()):
             # Find sum of products for denominator
             sop = 0
-            for j in range(len(nodes)):
+            for j in range(graph.number_of_nodes()):
                 sop = sop + centralities[j]*degrees[j]
             delta_black.append(budget*centralities[i]*degrees[i] / sop)
-
+                       
     elif TYPE == 'all':
         for i in range(len(nodes)):
             prev_net_exp = network_exposure(nodes)
@@ -79,10 +80,10 @@ def get_delta_black(nodes, adjacency, delta_red):
             for j in range(len(nodes)):
                 sop = sop + centralities[j]*degrees[j]*prev_net_exp[j]
             delta_black.append(budget*centralities[i]*degrees[i]*prev_net_exp[i] / sop)
+            
 
     return delta_black
-            
-    
+                
 
 def simulation(adjacency, runtime):
     nodes = build_network(adjacency)
@@ -94,7 +95,7 @@ def simulation(adjacency, runtime):
         
         # Get array of delta_black if update is required at each time step
         if TYPE == 'exposure' or TYPE == 'all':
-            delta_black = get_delta_black(nodes, adjacency, nodes[0].delta_red*len(nodes))
+            delta_black = get_delta_black(nodes, adjacency, nodes[0].delta_red)
                 
         # update infection rates
         avg_infection_rate.append(network_infection_rate(nodes))
